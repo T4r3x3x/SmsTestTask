6e0c3bc887dcf6fe7a61d55ce310979a9f6051e2
barsukov.mihail <barsukov.mihail@forward-development.ru>
2026-08-19 17:42:49 +0700

    Правка верстки

---
+++ src/Sms.WpfApp/Controls/RoundedClipBorder.cs
@@ -0,0 +1,20 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Sms.WpfApp.Controls;
+
+public sealed class RoundedClipBorder : Border
+{
+    protected override Size ArrangeOverride(Size finalSize)
+    {
+        var size = base.ArrangeOverride(finalSize);
+        if (Child is not null)
+        {
+            var radius = Math.Max(0, CornerRadius.TopLeft - BorderThickness.Left);
+            Child.Clip = new RectangleGeometry(new Rect(Child.RenderSize), radius, radius);
+        }
+
+        return size;
+    }
+}
